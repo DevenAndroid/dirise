0ab2b0321e0d8c098d3e54b5a8de0f89e52ef1d4
yasheoxys <yashagarwal@eoxysitsolutionllp.com>
2023-06-23 15:52:17 +0530

    23/06/2023 Aayushi

--- lib/screens/bottomavbar.dart
+++ lib/screens/bottomavbar.dart
@@ -4,11 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
-
 import '../controller/homepage_controller.dart';
 import '../widgets/common_colour.dart';
-
-
 
 class BottomNavbar extends StatefulWidget {
   const BottomNavbar({Key? key}) : super(key: key);
@@ -20,24 +17,17 @@ class BottomNavbar extends StatefulWidget {
 class _BottomNavbarState extends State<BottomNavbar> {
   final bottomController = Get.put(BottomNavBarController());
 
-
-
   final pages = [
     const HomePage(),
     LoginScreen(),
     LoginScreen(),
     LoginScreen(),
-
   ];
-
-
 
   @override
   Widget build(BuildContext context) {
     return Obx(() {
       return Scaffold(
-
-
         body: pages.elementAt(bottomController.pageIndex.value),
         extendBody: true,
         // extendBodyBehindAppBar: true,
@@ -62,11 +52,9 @@ class _BottomNavbarState extends State<BottomNavbar> {
                 blurRadius: 6.0,
               ),
             ],
-
           ),
           child: Column(
             children: [
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -75,24 +63,36 @@ class _BottomNavbarState extends State<BottomNavbar> {
                       padding: const EdgeInsets.only(bottom: 10),
                       onPressed: () {
                         bottomController.updateIndexValue(0);
-
                       },
-
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+
                         children: [
 
-                          bottomController. pageIndex.value == 0
-                              ?  SvgPicture.asset('assets/images/home.svg',
-                            color: AppTheme.secondaryColor,
+                          bottomController.pageIndex.value == 0
+                              ? Image.asset(
+                                  'assets/icons/home.png',
+                                  // height: 10,
+                                  color: Colors.black,
+                                )
+                              : Image.asset(
+                                  'assets/icons/home.png',
+                            color: AppTheme.buttonColor,
+                                ),
+                          bottomController.pageIndex.value == 0
+                              ? Text(
+                                  "Home",
+                                  style: TextStyle(
+                                      color: AppTheme.secondaryColor,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400),
+                                )
+                              : Text(
+                            "  Home",
+                            style: TextStyle(
+                                color: AppTheme.buttonColor,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400),
                           )
-                              : SvgPicture.asset('assets/images/home.svg',),
-
-
-                          bottomController. pageIndex.value == 0
-                              ?
-                          Text("  Home",style: TextStyle(color: AppTheme.secondaryColor,fontSize: 15,fontWeight: FontWeight.w400),):Text("")
-
                         ],
                       ),
                     ),
@@ -102,19 +102,17 @@ class _BottomNavbarState extends State<BottomNavbar> {
                       padding: const EdgeInsets.only(bottom: 10),
                       onPressed: () {
                         bottomController.updateIndexValue(1);
-
-
                       },
                       child: Column(
                         children: [
-
-                          bottomController. pageIndex.value == 1
-                              ? SvgPicture.asset('assets/images/card.svg',
-                            color: AppTheme.secondaryColor,
-                          )
-                              :  SvgPicture.asset('assets/images/card.svg',),
-
-
+                          bottomController.pageIndex.value == 1
+                              ? SvgPicture.asset(
+                                  'assets/images/card.svg',
+                                  color: AppTheme.secondaryColor,
+                                )
+                              : SvgPicture.asset(
+                                  'assets/images/card.svg',
+                                ),
                         ],
                       ),
                     ),
@@ -125,21 +123,18 @@ class _BottomNavbarState extends State<BottomNavbar> {
                       child: MaterialButton(
                         padding: const EdgeInsets.only(bottom: 10),
                         onPressed: () {
-
                           bottomController.updateIndexValue(2);
-
-
-
                         },
                         child: Column(
                           children: [
-
                             bottomController.pageIndex.value == 2
-                                ?  SvgPicture.asset('assets/images/Wallet.svg',
-                              color: AppTheme.secondaryColor,
-                            )
-                                : SvgPicture.asset('assets/images/Wallet.svg',),
-
+                                ? SvgPicture.asset(
+                                    'assets/images/Wallet.svg',
+                                    color: AppTheme.primaryColor,
+                                  )
+                                : SvgPicture.asset(
+                                    'assets/images/Wallet.svg',
+                                  ),
                           ],
                         ),
                       ),
@@ -153,19 +148,18 @@ class _BottomNavbarState extends State<BottomNavbar> {
                       },
                       child: Column(
                         children: [
-
-                          bottomController. pageIndex.value == 3
-                              ?  SvgPicture.asset('assets/images/profile.svg',
-                            color: AppTheme.secondaryColor,
-                          )
-                              : SvgPicture.asset('assets/images/profile.svg',),
-
-
+                          bottomController.pageIndex.value == 3
+                              ? SvgPicture.asset(
+                                  'assets/images/profile.svg',
+                                  color: AppTheme.secondaryColor,
+                                )
+                              : SvgPicture.asset(
+                                  'assets/images/profile.svg',
+                                ),
                         ],
                       ),
                     ),
                   ),
-
                 ],
               ),
             ],
