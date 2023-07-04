43949ba158396f61bffd4a45500f38652db7fdbc
yasheoxys <yashagarwal@eoxysitsolutionllp.com>
2023-07-04 12:09:26 +0530

    4/07/2023 Aayushi

--- lib/widgets/common_textfield.dart
+++ lib/widgets/common_textfield.dart
@@ -57,62 +57,68 @@ class _CommonTextfieldState extends State<CommonTextfield> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(height: 65,
-      child: TextFormField(
-          style: const TextStyle(),
-          autofocus: false,
-          textInputAction: TextInputAction.next,
-          onFieldSubmitted: widget.onFieldSubmitted,
-          inputFormatters: widget.inputFormatters,
-          onChanged: widget.onChanged,
-          onEditingComplete: widget.onEditingCompleted,
-          obscureText: widget.obSecure,
-          minLines: widget.isMulti ? 4 : 1,
-          maxLines: widget.isMulti ? null : 1,
-          onTap: widget.onTap,
-          enabled: widget.enabled,
-          readOnly: widget.readOnly,
-          keyboardType: widget.keyboardType,
-          controller: widget.controller,
-          decoration: InputDecoration(
-            counterStyle: const TextStyle(
-              color: AppTheme.primaryColor,
-              fontSize: 25,
-            ),
-            counter: const Offstage(),
-            errorMaxLines: 2,
-            enabled: widget.enabled,
-            //   fillColor: Colors.transparent,
-            hintText: widget.hintText,
-            errorText: widget.errorText,
-            labelText: widget.labelText,
-            prefixIcon: widget.prefix,
-            suffixIcon: widget.suffixIcon,
-            hintStyle:  GoogleFonts.poppins(
-              color: AppTheme.primaryColor,
-              fontSize: 15,
-            ),
-            /*errorStyle: const TextStyle(
-                overflow: TextOverflow.clip,
-              // ),*/
-
-
-
-            border: InputBorder.none,
-            focusedBorder: const OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                borderSide: BorderSide(color: AppTheme.secondaryColor)),
-            disabledBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-              borderSide: BorderSide(color: AppTheme.secondaryColor),
-            ),
-
-            enabledBorder: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-              borderSide: BorderSide(color: AppTheme.secondaryColor),
-            ),
+    return TextFormField(
+        style: const TextStyle(),
+        autofocus: false,
+        textInputAction: TextInputAction.next,
+        onFieldSubmitted: widget.onFieldSubmitted,
+        inputFormatters: widget.inputFormatters,
+        onChanged: widget.onChanged,
+        onEditingComplete: widget.onEditingCompleted,
+        obscureText: widget.obSecure,
+        minLines: widget.isMulti ? 4 : 1,
+        maxLines: widget.isMulti ? null : 1,
+        onTap: widget.onTap,
+        enabled: widget.enabled,
+        readOnly: widget.readOnly,
+        keyboardType: widget.keyboardType,
+        controller: widget.controller,
+        decoration: InputDecoration(
+          counterStyle: const TextStyle(
+            color: AppTheme.primaryColor,
+            fontSize: 25,
           ),
-          validator: widget.validator),
-    );
+          counter: const Offstage(),
+          errorMaxLines: 2,
+          enabled: widget.enabled,
+          //   fillColor: Colors.transparent,
+          hintText: widget.hintText,
+          errorText: widget.errorText,
+          labelText: widget.labelText,
+          prefixIcon: widget.prefix,
+          suffixIcon: widget.suffixIcon,
+          hintStyle:  GoogleFonts.poppins(
+            color: AppTheme.primaryColor,
+            fontSize: 15,
+          ),
+          /*errorStyle: const TextStyle(
+              overflow: TextOverflow.clip,
+            // ),*/
+
+
+
+          border: InputBorder.none,
+         focusedErrorBorder: const OutlineInputBorder(
+             borderRadius: BorderRadius.all(Radius.circular(8)),
+             borderSide: BorderSide(color: AppTheme.secondaryColor)
+         ),
+          errorBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              borderSide: BorderSide(color: AppTheme.secondaryColor)
+          ),
+          focusedBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              borderSide: BorderSide(color: AppTheme.secondaryColor)),
+          disabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            borderSide: BorderSide(color: AppTheme.secondaryColor),
+          ),
+
+          enabledBorder: const OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            borderSide: BorderSide(color: AppTheme.secondaryColor),
+          ),
+        ),
+        validator: widget.validator);
   }
 }
