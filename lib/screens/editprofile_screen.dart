1b8032e68ce3c94d60ef13dc6f58318cb09408b3
yasheoxys <yashagarwal@eoxysitsolutionllp.com>
2023-07-13 15:40:58 +0530

    13-07-23 Aayushi

--- lib/screens/editprofile_screen.dart
+++ lib/screens/editprofile_screen.dart
@@ -15,7 +15,8 @@ import 'package:image_picker/image_picker.dart';
 import '../controller/profile_controller.dart';
 import '../model/profile_model.dart';
 import '../repoistery/repository.dart';
-import '../repoistery/update_profile_repo.dart';
+
+// import '../repoistery/update_profile_repo.dart';
 import '../utils/ApiConstant.dart';
 import '../utils/helper.dart';
 import '../widgets/common_colour.dart';
@@ -55,8 +56,7 @@ class _EditProfileState extends State<EditProfile> {
     profileController.getDataProfile();
   }
 
-  updateProfile(){
-
+  updateProfile() {
     if (formKeyProfile.currentState!.validate()) {
       Map map = <String, String>{};
       map['first_name'] = profileController.firstNameController.text.trim();
@@ -66,12 +66,9 @@ class _EditProfileState extends State<EditProfile> {
       map['phone'] = profileController.phoneController.text.trim();
       map['password'] = profileController.passwordController.text.trim();
 
-      repositories.multiPartApi(
-          mapData: map, images: {
-            "profile" : image
-      }, url: ApiUrls.updateProfile,
-        context: context
-      ).then((value) {
+      repositories
+          .multiPartApi(mapData: map, images: {"profile": image}, url: ApiUrls.updateProfile, context: context)
+          .then((value) {
         ModelCommonResponse response = ModelCommonResponse.fromJson(jsonDecode(value));
         showToast(response.message.toString());
 
@@ -84,9 +81,7 @@ class _EditProfileState extends State<EditProfile> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           title: Text(
@@ -106,149 +101,136 @@ class _EditProfileState extends State<EditProfile> {
           ),
         ),
         body: Obx(() {
-          return  profileController.statusOfProfile.value.isSuccess
-              ?
-
-            SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Form(
-                key: _formKey,
-                child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(shape: BoxShape.circle, border: Border.all(color: AppTheme.primaryColor)),
-                    child: Center(
-                      child: InkWell(
-                        onTap: () => pickImage(),
-                        child: image.path != ""
-                            ? ClipOval(
-                          child: Image.file(
-                            image,
-                            width: 120,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                            : Container(
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(100)),
-                            color: AppTheme.primaryColor,
-                          ),
-                          child: ClipOval(
-                            child: CachedNetworkImage(
-                              width: 120,
-                              height: 120,
-                              fit: BoxFit.cover,
-                              imageUrl: profileController.model.value.user!.profileImage.toString(),
-                              placeholder: (context, url) => const SizedBox(),
-                              errorWidget: (context, url, error) => const SizedBox(),
+          return profileController.statusOfProfile.value.isSuccess
+              ? SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration:
+                              BoxDecoration(shape: BoxShape.circle, border: Border.all(color: AppTheme.primaryColor)),
+                          child: Center(
+                            child: InkWell(
+                              onTap: () => pickImage(),
+                              child: image.path != ""
+                                  ? ClipOval(
+                                      child: Image.file(
+                                        image,
+                                        width: 120,
+                                        height: 120,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    )
+                                  : Container(
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.all(Radius.circular(100)),
+                                        color: AppTheme.primaryColor,
+                                      ),
+                                      child: ClipOval(
+                                        child: CachedNetworkImage(
+                                          width: 120,
+                                          height: 120,
+                                          fit: BoxFit.cover,
+                                          imageUrl: profileController.model.value.user!.profileImage.toString(),
+                                          placeholder: (context, url) => const SizedBox(),
+                                          errorWidget: (context, url, error) => const SizedBox(),
+                                        ),
+                                      ),
+                                    ),
                             ),
                           ),
                         ),
-                      ),
+                        Text(
+                          'Name',
+                          style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CommonTextfield(
+                          obSecure: false,
+                          hintText: 'Williams jones',
+                          controller: profileController.firstNameController,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Last Name',
+                          style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CommonTextfield(
+                          obSecure: false,
+                          hintText: 'Williams jones',
+                          controller: profileController.lastNameController,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Email',
+                          style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CommonTextfield(
+                          obSecure: false,
+                          hintText: 'williamsjones@gmail.com',
+                          controller: profileController.emailController,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Mobile number',
+                          style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        CommonTextfield(
+                          obSecure: false,
+                          hintText: '+91 987-654-3210',
+                          controller: profileController.phoneController,
+                        ),
+                        SizedBox(
+                          height: size.height * .3,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: size.height * .07,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: AppTheme.buttonColor,
+                                shape:
+                                    const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                                textStyle: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            onPressed: () {
+                              updateProfile();
+                            },
+                            child: Text(
+                              'Save',
+                              style:
+                                  GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ]),
                     ),
                   ),
-                  Text(
-                    'Name',
-                    style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CommonTextfield(
-                    obSecure: false,
-                    hintText: 'Williams jones',
-                    controller: profileController.firstNameController,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Last Name',
-                    style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CommonTextfield(
-                    obSecure: false,
-                    hintText: 'Williams jones',
-                    controller: profileController.lastNameController,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Email',
-                    style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CommonTextfield(
-                    obSecure: false,
-                    hintText: 'williamsjones@gmail.com',
-                    controller: profileController.emailController,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Mobile number',
-                    style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CommonTextfield(
-                    obSecure: false,
-                    hintText: '+91 987-654-3210',
-                    controller: profileController.phoneController,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Address',
-                    style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CommonTextfield(
-                      obSecure: false,
-                      hintText: 'Riverside Building, County hall,',
-                      controller: profileController.addressController),
-                  SizedBox(
-                    height: size.height * .3,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: size.height * .07,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: AppTheme.buttonColor,
-                          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                          textStyle: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          )),
-                      onPressed: () {
-                        updateProfile();
-                        // Get.toNamed(MyRouters.doctorNavbar);
-                      },
-                      child: Text(
-                        'Save',
-                        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ]),
-              ),
-            ),
-          ): profileController.statusOfProfile.value.isError
-              ?SizedBox()
-              : const CircularProgressIndicator();
+                )
+              : profileController.statusOfProfile.value.isError
+                  ? SizedBox()
+                  : const CircularProgressIndicator();
         }));
   }
 }
